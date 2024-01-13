2bfe4977af8fa6c2bec1c701a70d35289769e8db
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2024-01-13 17:59:39 -0300

    Add project files.

---
+++ MeuProjeto/Aluno.cs
@@ -0,0 +1,36 @@
+﻿namespace MeuProjeto;
+
+internal class Aluno
+{
+    public string? Nome { get; set; }
+    public double Nota { get; set; }
+
+    public static List<Aluno> geraAlunos()
+    {
+        List<Aluno> alunos = new List<Aluno>()
+        {
+           new Aluno() {Nome="Maria", Nota=8.75},
+           new Aluno() {Nome="Manoel",Nota=6.95},
+           new Aluno() {Nome="Amanda",Nota=7.25},
+           new Aluno() {Nome="Carlos",Nota=6.55},
+           new Aluno() {Nome="Jaime",Nota=8.5},
+           new Aluno() {Nome="Debora",Nota=5.95},
+           new Aluno() {Nome="Alicia",Nota=9.25},
+           new Aluno() {Nome="Sandra",Nota=5.55},
+           new Aluno() {Nome="Marta",Nota=7.85},
+           new Aluno() {Nome="Sueli",Nota=9.15},
+        };
+        return alunos;
+    }
+
+    public static void CalculaMedia(List<Aluno> alunos)
+    {
+        double somaDasNotas = 0;
+        foreach (Aluno item in alunos)
+        {
+            somaDasNotas += item.Nota;
+        }
+        double mediaDasNotas = somaDasNotas / alunos.Count();
+        Console.WriteLine($"\nA média das notas é de: {mediaDasNotas}");
+    }
+}
---
+++ MeuProjeto/Program.cs
@@ -0,0 +1,32 @@
+﻿using MeuProjeto;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Aluno> listaDeAlunos = Aluno.geraAlunos();  //Chamando o método que retorna uma lista de objetos do tipo Aluno e armazenando em listaDeAlunos
+
+        Console.WriteLine("Lista de Alunos e suas respectivas notas: \n");
+        mostraAlunos(listaDeAlunos);                                              //Chamando método para mostrar alunos
+
+        Aluno.CalculaMedia(listaDeAlunos);                                        //Chamando método que calcula a média das notas
+
+        listaDeAlunos.Add(new Aluno() { Nome = "Bia", Nota = 7.75 });             //Adicionando novos alunos
+        listaDeAlunos.Add(new Aluno() { Nome = "Mario", Nota = 8.95 });
+
+        var maioresNotas = listaDeAlunos.FindAll(n => n.Nota >= 8.0);                     // Filtrando as maiores notas
+        var maioresNotasOrdenadas = maioresNotas.OrderByDescending(n => n.Nota).ToList(); //Ordenando alunos por notas
+        Console.WriteLine("\nAlunos com as maiores notas: \n");
+        mostraAlunos(maioresNotasOrdenadas);
+
+    }
+
+    public static void mostraAlunos(List<Aluno> lista)
+    {
+        foreach (Aluno item in lista)
+        {
+            Console.WriteLine($"Nome: {item.Nome}\t Nota: {item.Nota}");
+        }
+
+    }
+}
